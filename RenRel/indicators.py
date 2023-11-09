e7c5c8a68bbbcfc426b4ac9b8cf0975c74ed6831
jusrojasrod <rebelion.96@gmail.com>
2023-11-09 11:19:17 -0500

    added bollinger bands

--- RenRel/indicators.py
+++ RenRel/indicators.py
@@ -41,3 +41,31 @@ def _PM(prices_df, sma_df):
     PM = (prices_df - sma_df) / sma_df
 
     return PM
+
+
+def _bollinger_bands(data, periods=14, n=1.5,
+                     column_name='close'):
+    """
+    It uses close prices.
+    Parameters
+    ----------
+    data: pandas dataframe
+        prices series data.
+    periods: int
+        Lockback period. 14 by default.
+    n: int
+        Number of standart deviations up and dowm from
+        the std. 1.5 by default.
+    """
+    sma = _sma(data=data, period=periods)
+    std = data.rolling(window=periods).std()
+    upBand = sma + n*std
+    downBand = sma - n*std
+
+    return pd.DataFrame(index=data.index,
+                        data={'close': data,
+                              'sma': sma,
+                              'Upper Band': upBand,
+                              'Lower Band': downBand
+                              }
+                        )
