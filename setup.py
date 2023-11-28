9c1a467f7af75a098a2a4bdc78834efda79e9188
jusrojasrod <rebelion.96@gmail.com>
2023-11-28 11:44:32 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+VERSION = "0.0.1"
+
+
+setup(name='relper',
+      packages=find_packages(),
+      version=VERSION,
+      author="Sebastian Rojas")
