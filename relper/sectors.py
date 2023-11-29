1d2172e53e5b389783b48dbae3f010f16bb0a55b
jusrojasrod <rebelion.96@gmail.com>
2023-11-29 10:24:02 -0500

    Create sectors.py

---
+++ relper/sectors.py
@@ -0,0 +1,19 @@
+ETF_sectors = {
+    "XLB": "Materials",
+    "XLC": "Telecom",
+    "XLE": "Energy",
+    "XLF": "Finantials",
+    "XLI": "Industrials",
+    "XLK": "Technology",
+    "XLRE": "Real Estate",
+    "XLP": "Consumer Staples",
+    "XLU": "Utilities",
+    "XLV": "Healthcare",
+    "XLY": "Consumer Discretionary",
+    "MDY": "mid-sized companies",
+    "SLY": "Small Cap ETF",
+    "TLT": "20+ Year Treasury Bond",
+    "VEA": "Developed Markets Index",
+    "VWO": "Emerging Markets Index",
+    "SPY": "S&P 500"
+}
